f401b6a09866fd9eece4b1c62146c17515f1b517
Moony-H <moonylight247@gmail.com>
2024-03-25 22:47:50 +0900

    module directory

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PetsGo"
+include(":app")
+include(":feature:feed")
+include(":core")
+include(":core:common")
