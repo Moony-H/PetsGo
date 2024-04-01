5057dd534c5352c9a0e9d47b2985fd8e71873ed5
Moony-H <moonylight247@gmail.com>
2024-04-01 23:48:47 +0900

    toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,20 +51,27 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    //noinspection UseTomlInstead
     implementation("androidx.compose.ui:ui")
+    //noinspection UseTomlInstead
     implementation("androidx.compose.ui:ui-graphics")
+    //noinspection UseTomlInstead
     implementation("androidx.compose.ui:ui-tooling-preview")
+    //noinspection UseTomlInstead
     implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    //noinspection UseTomlInstead
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    //noinspection UseTomlInstead
     debugImplementation("androidx.compose.ui:ui-tooling")
+    //noinspection UseTomlInstead
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation(project(":feature:test_home"))
